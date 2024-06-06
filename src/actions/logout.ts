36f98361cc17789aaff2635ca41ac95c718203b7
FThappy <thangchu766@gmail.com>
2024-06-07 03:35:48 +0700

    new

--- src/actions/logout.ts
+++ src/actions/logout.ts
@@ -1,21 +1,45 @@
 "use server"
-import { userRequest } from "@/utils/requestMethod";
+// import { userRequest } from "@/utils/requestMethod";
 import { cookies } from "next/headers";
+
+// export const logout = async () => {
+//     const cookie = cookies().get("Authorization");
+//   try {
+//     const res = await userRequest.delete(`api/auth/logout`, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Cookie: `Authorization=${cookie?.value}`,
+//       },
+//       credentials: "include",
+//     });
+//     return res.data;
+//   } catch (error: any) {
+//     console.log(error);
+//     return error.response.data;
+//   }
+// };
 
 export const logout = async () => {
     const cookie = cookies().get("Authorization");
 
-    console.log(cookie)
-  try {
-    const res = await userRequest.delete(`api/auth/logout`, {
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `Authorization=${cookie?.value}`,
-      },
-    });
-    return res.data;
-  } catch (error: any) {
-    console.log(error);
-    return error.response.data;
-  }
+    try {
+        const response = await fetch("https://datn-be-3cll.onrender.com/api/auth/logout", {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Cookie: `Authorization=${cookie?.value}`,
+          },
+          credentials: "include",
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error: any) {
+        console.error(error);
+        return { error: error.message };
+    }
 };
--- src/utils/requestMethod.ts
+++ src/utils/requestMethod.ts
@@ -3,6 +3,17 @@ import { io } from "socket.io-client";
 
 
 
+// export const userRequest = axios.create({
+//   withCredentials: true,
+//   baseURL: process.env.NEXT_PUBLIC_LINK_SERVER,
+// });
+
+
+// export const socket = io("http://localhost:5000", {
+//   withCredentials: true,
+// });
+
+
 export const userRequest = axios.create({
   withCredentials: true,
   baseURL: "https://datn-be-3cll.onrender.com",
@@ -12,8 +23,5 @@ export const userRequest = axios.create({
 export const socket = io("https://datn-be-3cll.onrender.com", {
   withCredentials: true,
 });
-// export const socket = io("http://localhost:5000", {
-//   withCredentials: true,
-// });
 
 
