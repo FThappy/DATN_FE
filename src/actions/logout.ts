022f45937600afb4cad6dbfafd87ad2e7ca9923b
FThappy <thangchu766@gmail.com>
2024-06-07 13:08:01 +0700

    new

--- src/actions/logout.ts
+++ src/actions/logout.ts
@@ -3,20 +3,20 @@ import { userRequest } from "@/utils/requestMethod";
 import { cookies } from "next/headers";
 
 export const logout = async () => {
-    const cookie = cookies().get("Authorization");
-  try {
-    const res = await userRequest.delete(`api/auth/logout`, {
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `Authorization=${cookie?.value}`,
-      },
-      credentials: "include",
-    });
-    return res.data;
-  } catch (error: any) {
-    console.log(error);
-    return error.response.data;
-  }
+  const cookie = cookies().get("Authorization");
+  console.log(cookie);
+  // try {
+  //   const res = await userRequest.delete(`api/auth/logout`, {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Cookie: `Authorization=${cookie?.value}`,
+  //     },
+  //   });
+  //   return res.data;
+  // } catch (error: any) {
+  //   console.log(error);
+  //   return error.response.data;
+  // }
 };
 
 // export const logout = async () => {
