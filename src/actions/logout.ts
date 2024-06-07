29955da8016329c279400e449da4da2939dcae23
FThappy <thangchu766@gmail.com>
2024-06-07 15:06:47 +0700

    new

--- src/actions/logout.ts
+++ src/actions/logout.ts
@@ -5,22 +5,21 @@ import { cookies } from "next/headers";
 export const logout = async () => {
   const cookie = cookies();
   const auth = cookie.get("Authorization");
-
   console.log(cookie.getAll());
-  console.log(cookie)
+  console.log(cookie.get("_vercel_jwt"));
 
-  try {
-    const res = await userRequest.delete(`api/auth/logout`, {
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `Authorization=${auth?.value}`,
-      },
-    });
-    return res.data;
-  } catch (error: any) {
-    console.log(error);
-    return error.response.data;
-  }
+//   try {
+//     const res = await userRequest.delete(`api/auth/logout`, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Cookie: `Authorization=${auth?.value}`,
+//       },
+//     });
+//     return res.data;
+//   } catch (error: any) {
+//     console.log(error);
+//     return error.response.data;
+//   }
 };
 
 // export const logout = async () => {
