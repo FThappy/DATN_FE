b1a84e6e131cc8c74e911c2804e9a2e79cce2587
FThappy <thangchu766@gmail.com>
2024-06-16 17:26:39 +0700

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,19 +11,6 @@ const robotoCondensed = Roboto_Condensed({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Chung Tay",
   description: "Trang web từ thiện",
-  openGraph: {
-    title: "Chung Tay",
-    description: "Trang web từ thiện",
-    url: "https://datn-fe-3xyo.onrender.com/",
-    siteName: "Next.js",
-    images: [
-      {
-        url: "/logo.png", // Must be an absolute URL
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
 };
 
 export default function RootLayout({
