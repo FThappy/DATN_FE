86a1596ee66bf725efaa91b01e8cc18a5b8b21fe
FThappy <thangchu766@gmail.com>
2024-06-27 15:46:44 +0700

    fix

--- src/utils/requestMethod.ts
+++ src/utils/requestMethod.ts
@@ -3,24 +3,24 @@ import { io } from "socket.io-client";
 
 
 
-export const userRequest = axios.create({
-  withCredentials: true,
-  baseURL: process.env.NEXT_PUBLIC_LINK_SERVER,
-});
-
-
-export const socket = io("http://localhost:5000", {
-  withCredentials: true,
-});
-
-
-
-
 // export const userRequest = axios.create({
 //   withCredentials: true,
-//   baseURL: "https://datn-be-zrcv.onrender.com",
+//   baseURL: process.env.NEXT_PUBLIC_LINK_SERVER,
 // });
 
-// export const socket = io("https://datn-be-zrcv.onrender.com", {
+
+// export const socket = io("http://localhost:5000", {
 //   withCredentials: true,
 // });
+
+
+
+
+export const userRequest = axios.create({
+  withCredentials: true,
+  baseURL: "https://datn-be-zrcv.onrender.com",
+});
+
+export const socket = io("https://datn-be-zrcv.onrender.com", {
+  withCredentials: true,
+});
