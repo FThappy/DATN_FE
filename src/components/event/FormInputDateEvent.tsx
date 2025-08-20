baffdaec6c6310cb11ffe28d0f945f739629b90b
thang.chu <thang.chu@bmbsoft.com.vn>
2025-08-20 17:43:37 +0700

    update create event

--- src/components/event/FormInputDateEvent.tsx
+++ src/components/event/FormInputDateEvent.tsx
@@ -19,9 +19,9 @@ const FormInputDateEvent = (props: Props) => {
   return (
     <div className='flex w-full items-center gap-2 mt-2'>
       {' '}
-      <Popover>
-        <PopoverTrigger asChild>
-          <div className='flex border-2 border-gray-400 bg-white items-center h-[4rem]  gap-2 rounded-[12px] p-2 px-4  w-1/2'>
+      <Popover defaultOpen>
+        <PopoverTrigger className="w-full" >
+          <div className='w-full flex border-2 border-gray-400 bg-white items-center h-[4rem]  gap-2 rounded-[12px] p-2 px-4  '>
             <FaCalendarAlt size={24} color='gray' />
             <div className='flex flex-col gap-1 ml-2 items-start'>
               <p className='text-[0.8rem]'>{detail}</p>
@@ -46,13 +46,13 @@ const FormInputDateEvent = (props: Props) => {
           />
         </PopoverContent>
       </Popover>
-      <div className='w-1/2 p-2 px-4 border-2 border-gray-400 rounded-[12px] h-[4rem] flex flex-col '>
+      <div className='w-full p-2 px-4 border-2 border-gray-400 rounded-[12px] h-[4rem] flex flex-col '>
         <p className='text-[0.8rem]'>Thời gian bắt đầu</p>
         <input
           type='time'
           id='timeStart'
           name='timeStart'
-          className='p-2 outline-none	'
+          className='p-2 outline-none	bg-transparent'
           value={format(time, 'HH:mm')}
           onChange={(e: ChangeEvent<HTMLInputElement>) => {
             e.preventDefault();
